edc6899c0d55b5d7d0becd1b5e6338f863938e40
minasis <nagisa@naver.com>
2018-08-04 21:11:47 +0900

    1. 점수영역 swiper추가

--- simpleVue/src/components/main/scores.vue
+++ simpleVue/src/components/main/scores.vue
@@ -5,32 +5,11 @@
             <ul>
                 <li>
                     <div id='prev_score_noti'></div>
-                    <vue-circle
-                        :progress="50"
-                        :size="100"
-                        :reverse="false"
-                        line-cap="round"
-                        :fill="fill"
-                        empty-fill="rgba(0, 0, 0, .1)"
-                        :animation-start-value="0.0"
-                        :start-angle="0"
-                        insert-mode="append"
-                        :thickness="5"
-                        :show-percent="true"
-                        @vue-circle-progress="progress"
-                        @vue-circle-end="progress_end">
-                        <p>Slot!</p>
-                    </vue-circle>
-                    <div class="circleGragh">
-                        <b class="num ">48</b>
-                        <!-- <div class="myPosition">
-                            <div class="swiper-container">
-                                <div class="swiper-wrapper">
-                                    <span class="swiper-slide"></span>
-                                    <span class="swiper-slide">5 등</span>	
-                                </div>
-                            </div>
-                        </div> -->
+                    <b class="num ">48</b>
+                    <div class="myPosition">
+                        <swiper :options="swiperOption" rel="mySwiper" >
+                            <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">{{slide}}</swiper-slide>
+                        </swiper>
                     </div>
                 </li>
                 <li></li>
@@ -43,22 +22,44 @@
 
 <script>
 import VueCircle from 'vue2-circle-progress'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
     components: {
-      VueCircle
+      VueCircle,
+      swiper,
+      swiperSlide
     },
     data(){
         return{
             fill : { gradient: ["red", "green", "blue"] },
+            swiperSlides:['등수 없음'],
+            swiperOption: {
+                loop: true,
+				allowTouchMove:false,
+				speed:800
+            },
+            
         }
     },
-     methods:{
-      progress(event,progress,stepValue){
-        console.log(stepValue);
-      },
-      progress_end(event){
-        console.log("Circle progress end");
-      }
+    created: function(){
+        var obj = this;
+        var autoPlay = {
+            delay: 1500,
+			disableOnInteraction: false,
+        }
+        if(obj.swiperSlides.length > 1){
+            obj.swiperOption.autoplay = autoPlay
+        }
+    },
+    methods:{
+        progress(event,progress,stepValue){
+            console.log(stepValue);
+        },
+        progress_end(event){
+            console.log("Circle progress end");
+        }
     }
 }
 </script>
@@ -70,14 +71,15 @@ export default {
     .totalScore {padding: 30px 0px; border-bottom: 1px solid #e5e5e5; margin-bottom: 12px; position: relative;}
     .totalScore ul {font-size: 0px; line-height: 0px; display: inline-block; margin-right: 4px; width:100%;display: flex; align-items:center}
     .totalScore ul li {display: inline-block;position: relative; width:calc((100% - 152px) / 3); text-align: center;}
-    .totalScore ul li:first-child{margin-right: 10px; width: 126px; display: flex; justify-content:center; position: relative;}
+    .totalScore ul li:first-child{margin-right: 10px; width: 126px; position: relative;}
     .totalScore ul li:nth-child(2) {text-align: left;}
     .totalScore ul li:nth-child(4) {text-align: right;}
     .totalScore ul li:first-child .upDown {left:84px; margin-left: auto;}
 
-    .totalScore .circleGragh {width:126px;display: block; position: relative;}
-    .totalScore .circleGragh strong {font-size: 0.8rem; line-height: 1rem;}
-    .totalScore .circleGragh b {font-size:5.2rem; text-align: center; letter-spacing: -0.07rem; display: block; height: 70px;}
-    .totalScore .circleGragh b:after {position:relative;bottom:2px;font-size:2.5rem; content:'점';font-family:'AppleSDGothicNeo-Regular', 'nanumBarunGothic-regular'; margin-left: 3px; }
-    .totalScore .circleGragh b.data:after {display: inline}
+    .totalScore li:first-child b {font-size:5.2rem; width:100%; text-align: center; letter-spacing: -0.07rem; display: block; height: 70px;}
+    .totalScore li:first-child b:after {position:relative;bottom:2px;font-size:2.5rem; content:'점';font-family:'AppleSDGothicNeo-Regular', 'nanumBarunGothic-regular'; margin-left: 3px; }
+    .totalScore li:first-child b.data:after {display: inline}
+
+    .myPosition {height: 27px; border-radius: 17px; background: #f7f7f7; display: inline-block; margin-top: -1rem; width: 82px; text-align: center;font-size: 1.4rem; color:#666; line-height: 29px; }
+    .myPosition .swiper-slide {text-align: center; font-size: 1.4rem; color:#666; line-height: 29px; padding:0 12px; height: 27px; white-space: nowrap}
 </style>
